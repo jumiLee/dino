3f85df3d727aecec55f78cb9a02c9ad839322e93
vassystem <worldleader77@gmail.com>
2020-09-18 19:40:38 +0800

    no message

--- src/main/java/first/vassystem/dto/ItemList.java
+++ src/main/java/first/vassystem/dto/ItemList.java
@@ -3,8 +3,8 @@ package first.vassystem.dto;
 public class ItemList {
 	public int itemId;			//item id
 	public String itemNm;		//item name
-	public int itemCategory;	//item category (10:food, 20:color)
-	public int itemType;		//item type (0:none, 1:meat, 2:vegetable, 3:fruit)
+	public int itemCategory;	//item category (0:none, 1:meat, 2:vegetable, 3:fruit)
+	public int itemType;		//item type (10:food, 20:color)
 	public int itemPrice;		//item price
 	public int unit_cd;			//item price unit
 	public String itemDesc;
